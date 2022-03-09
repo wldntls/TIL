9de0766afa58daf532bfb7451d861a69f7e2a3e4
wldntls <sddyy1004@naver.com>
2022-03-09 22:01:54 +0900

    220309_algorithm

--- algorithm/DAY.02/실습 문제/BOJ_1032.py
+++ algorithm/DAY.02/실습 문제/BOJ_1032.py
@@ -2,7 +2,8 @@ N = int(input())
 
 file_list = [input() for i in range(N)]
 
-for i in file_list:
-    if file_list[i] != file_list[i+1]:
-        print(file_list)
+for file in file_list:
+    for i in file:
+        if file_list[i] != file_list[i+1]:
+            print(file_list)
 
