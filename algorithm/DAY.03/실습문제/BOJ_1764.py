8fb68ebf8a9a5b901be92dcd01ef71cfc751098e
wldntls <sddyy1004@naver.com>
2022-03-15 21:52:33 +0900

    220315_algorithm

--- algorithm/DAY.03/실습문제/BOJ_1764.py
+++ algorithm/DAY.03/실습문제/BOJ_1764.py
@@ -1,15 +1,19 @@
-n, m = map(int, input().split())
+n, m = map(int, input().split()) # n, m를 공백을 기준으로 정수로 입력받기
 
-dict = {input(): 1 for _ in range(n)}
-people_list = []
+dict = {input(): 1 for _ in range(n)} # 듣도 못한 사람의 수 n만큼을 딕셔너리의 key값으로 입력받고, value값은 1로 설정
+people_list = [] # for문에서 사용할 리스트 변수 선언
 
+for i in range(m): # 보도 못한 사람의 수 m만큼 for문 돌리기
+    people = input() # 문자 입력받기
+    if people in dict.keys(): # 만약 입력받은 people이 dict 딕셔너리 key값에 있다면
+        people_list.append(people) # for문 밖에서 선언한 people_list 리스트에 하나씩 삽입
+print(len(people_list)) # 리스트의 길이를 출력하는 함수인 len를 활용하여 듣보잡의 수를 출력
+print('\n'.join(sorted(people_list))) # join함수를 활용하여 단어마다 개행을 넣어주고, sorted함수를 활용하여 이름을 사전순으로 출력
 
-for i in range(m):
-    people = input()
-    if people in dict.keys():
-        people_list.append(people)
-print(len(people_list))
-print('\n'.join(sorted(people_list)))
-
-
-
+############################
+# 코드 리뷰
+# - 리스트에 명단을 모두 넣어서 문제를 풀려고 했었음
+# - 하지만 시간복잡도 문제와 list 범위 밖을 넘어가는 오류 문제가 발생했었음
+# - 그래서 딕셔너리를 활용하여 문제를 풀 수 있는 방안을 생각했음
+# - 결과적으로 리스트를 활용한 것보다 훨씬 더 쉽게 결과값을 출력할 수 있었음
+############################
--- algorithm/DAY.03/실습문제/BOJ_5622.py
+++ algorithm/DAY.03/실습문제/BOJ_5622.py
@@ -5,14 +5,16 @@ dial = {'ABC': 3,
         'MNO': 7,
         'PQRS': 8,
         'TUV': 9,
-        'WXYZ': 10}
+        'WXYZ': 10} # 숫자와 문자가 각각 1:1로 매핑되어 있기 때문에 딕셔너리로 각각의 값을 입력해줌
 
-word = input()
+word = input() # 문자열을 입력받기
 
-min_time = 0 
+min_time = 0 # for문 안에서 숫자를 더하기 위한 변수
 
-for key, value in dial.items():
-    for i in word:
-        if i in key:
-            min_time += value
-print(min_time)
+for key, value in dial.items(): # items를 활용하여 key값과 value값을 동시에 받아 for문 돌리기
+    for i in word: # 입력받은 word를 for문을 돌려 알파벳 하나씩 돌리기
+        if i in key: # 만약에 i가 key값에 있다면
+            min_time += value # min_time에 value값을 더해줌
+print(min_time) # 최종값을 출력
+ 
+
