05d0f78f642aac5d4f9b0aae9d37f7afea7e6bcd
wldntls <sddyy1004@naver.com>
2022-03-29 23:32:16 +0900

    220329_algorithm

--- algorithm/week3, 4/BOJ_16967.py
+++ algorithm/week3, 4/BOJ_16967.py
@@ -1,28 +1,33 @@
-# 미해결
 h, w, x, y = map(int, input().split())
 
 merge_matrix=[list(map(int, input().split())) for _ in range(h+x)]
 
-# 배열 B
-print(merge_matrix)
-
-a_matrix = []
-
 # 배열 A의 행렬을 이중 리스트 형태로 추출해내야함?
 a_matrix = [[0 for _ in range(w)] for _ in range(h)]
+check = [[0 for _ in range(w)] for _ in range(h)]
+
 print(a_matrix)
 
 for i in range(h):
     for j in range(w):
         # 배열 A와 배열 B가 겹치는 구간 찾기
         if i < h and j < w:
-            a_matrix[i][j] += 1
+            check[i][j] += 1
         if i+x < h and j+y < w:
-            a_matrix[i+x][j+y] += 1
-print(a_matrix)
+            check[i+x][j+y] += 1
 
 # 겹치는 구간을 원래 배열 A로 돌려주기
+for i in range(h):
+    for j in range(w):
+        if check[i][j] == 1:
+            a_matrix[i][j] = merge_matrix[i][j]
+        elif check[i][j] == 2:
+            a_matrix[i][j] = merge_matrix[i][j] - a_matrix[i-x][j-y]
 
+for i in range(h):
+    for j in range(w):
+        print(a_matrix[i][j], end=' ')
+    print()
 
             
 
