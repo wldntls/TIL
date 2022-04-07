a8a1dd4c950e4e41638e17c191e83f01875a7a7e
wldntls <sddyy1004@naver.com>
2022-04-07 16:05:56 +0900

    220407_algorithm

--- algorithm/week 3, 4/free.py
+++ algorithm/week 3, 4/free.py
@@ -1,32 +1,10 @@
-from collections import deque
+d = [0] * 100
 
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
+d[1] = 1
+d[2] = 1
+n = 99
 
-    while queue:
-        v = queue.popleft()
-        print(v, end = ' ')
+for i in range(3, n + 1):
+	d[i] = d[i - 1] + d[i - 2]
 
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-
-# 각 노드가 연결된 정보를 표현(2차원 리스트)
-graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7]
-]
-# 각 노드가 방문된 정보를 표현(1차원 리스트)
-visited = [False] * 9
-
-# 정의된 bfs 함수 호출
-bfs(graph, 1, visited)
+print(d[n])
