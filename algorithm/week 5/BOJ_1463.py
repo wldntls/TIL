e25dc75c2873d5d8347bbfadd9ee20fe2f5f6d7a
wldntls <sddyy1004@naver.com>
2022-04-09 01:56:24 +0900

    220409_algorithm

--- algorithm/week 5/BOJ_1463.py
+++ algorithm/week 5/BOJ_1463.py
@@ -6,17 +6,17 @@
 n = int(input())
 
 dp = [0] * (n+1)
-print(dp)
+#print(dp)
 
 for i in range(2, n+1):
-    print(i)
+    #print(i)
     # 현재의 수에서 1을 빼는 경우
     dp[i] = dp[i - 1] + 1
     if n % 3 == 0:
         dp[i] = min(dp[i], dp[i//3] + 1)
     if n % 2 == 0:
         dp[i] = min(dp[i], dp[i//2] + 1)
-    print(dp)
+    #print(dp)
 
 print(dp[n])
 
