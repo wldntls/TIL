64fb6e1535292da0f995e20c6291ba8a4443d999
wldntls <sddyy1004@naver.com>
2022-04-09 13:40:46 +0900

    220409_algorithm

--- algorithm/coding_with_python/E_company_algorithm_compition.py
+++ algorithm/coding_with_python/E_company_algorithm_compition.py
@@ -16,4 +16,3 @@ while True:
     break
 
 print(count)
-
--- algorithm/week 5/BOJ_1991.py
+++ algorithm/week 5/BOJ_1991.py
@@ -1,43 +1,34 @@
-class Node:
-    def __init__(self, data, left_node, right_node):
-        self.data = data
-        self.left_node = left_node
-        self.right_node = right_node
-
-# 전위 순회(Predorder Traversal)
-def pre_order(node):
-    print(node.data, end = ' ')
-    if node.left_node != None:
-        pre_order(tree[node.left_node])
-    if node.right_node != None:
-        pre_order(tree[node.right_node])
-
-# 중위 순회(Inorder Traversal)
-def in_order(node):
-    if node.left_node != None:
-        in_order(tree[node.left_node])
-    print(node.data, end = ' ')
-    if node.right_node != None:
-        in_order(tree[node.right_node])
-
-# 후위 순회(Postorder Traversal)
-def post_order(node):
-    if node.right_node != None:
-        post_order(tree[node.right_node])
-    print(node.data, end = ' ')
-    if node.left_node != None:
-        post_order(tree[node.left_node])
-
-
 n = int(input())
 tree = {}
 
 for i in range(n):
     data, left_node, right_node = input().split()
-    if left_node == '.':
-        left_node = None
-    if right_node == '.':
-        right_node = None
-    tree[data] = Node(data, left_node, right_node)
+    tree[data] = [left_node, right_node] # 딕셔너리 형태를 key 루트 : value 왼쪽 자식, 오른쪽 자식
 print(tree)
 
+# 전위 순회(Predorder Traversal)
+def pre_order(node):
+    if node != '.':
+        print(node, end = '') # 1. 자기 자신 노드
+        pre_order(tree[node][0]) # 2. 왼쪽 자식 노드
+        pre_order(tree[node][1]) # 3. 오른쪽 자식 노드
+
+# 중위 순회(Inorder Traversal)
+def in_order(node):
+    if node != '.':
+        in_order(tree[node][0]) # 1. 왼쪽 자식 노드
+        print(node, end = '') # 2. 자기 자신 노드
+        in_order(tree[node][1]) # 3. 오른쪽 자식 노드
+
+# 후위 순회(Postorder Traversal)
+def post_order(node):
+    if node != '.':
+        post_order(tree[node][0])#  1. 왼쪽 자식 노드
+        post_order(tree[node][1]) # 2. 오른쪽 자식 노드
+        print(node, end = '') # 3. 자기 자신 노드
+
+pre_order('A')
+print()
+in_order('A')
+print()
+post_order('A')
