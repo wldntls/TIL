ba6619617e2d58d3d7d6abd07f4d177ebf3d32b2
wldntls <sddyy1004@naver.com>
2022-04-27 23:22:46 +0900

    220427_algorithm

--- algorithm/week 6/BOJ_10844.py
+++ algorithm/week 6/BOJ_10844.py
@@ -1,6 +1,15 @@
 # 계단의 수는 0~9까지 일 수밖에 없음. 왜냐하면, 10이 되면 1, 0 이렇게 되기 때문에
 # n = 1 일때, 길이가 1인 계단 수가 총 몇개 있는지 
 
+#         0  1  2  3  4  5  6  7  8  9
+# 자리수(1) 0  1  1  1  1  1  1  1  1  1
+# 자리수(2) 1  1  2  2  2  2  2  2  2  1
+# 자리수(3) 1  3  3  4  4  4  4  4  3  2
+
+# 규칙
+# 해당 위치의 대각선 위 위치의 숫자들이 합
+
+
 n = int(input())
 
 stairs = [[0]*10 for _ in range(n + 1)]
@@ -9,15 +18,26 @@ stairs = [[0]*10 for _ in range(n + 1)]
 stairs[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 for i in range(2, n + 1):
+
     # 계단 수가 0으로 끝나는 경우
     stairs[i][0] = stairs[i - 1][1]
-    print(stairs[i][0])
+    #print(stairs[i][0], 'a')
+    #print(stairs[i - 1][1],'b')
+
     # 계단 수가 9로 끝나는 경우
     stairs[i][9] = stairs[i - 1][8]
+    #print(stairs[i][9], 'c')
+    #print(stairs[i - 1][8], 'd')
 
     # 계단 수가 1~8로 끝나는 경우 
     for j in range(1, 9):
         stairs[i][j] = stairs[i - 1][j - 1] + stairs[i - 1][j + 1]
-        print(stairs[i][j])
+        # print(stairs[i - 1][j - 1], 'e')
+        # print(stairs[i - 1][j + 1], 'f')
+        # print(stairs[i][j], 'g')
 
-print(sum(stairs[n]) % 100000000)
+print(sum(stairs[n]) % 1000000000)
+
+# 아직 해결되지 않은 점 
+# tairs[i - 1][8] 등에서 왜 모두 i-1이지?
+# 출력은 왜 저렇게 뽑지? 문제에서 저렇게 뽑으라함
