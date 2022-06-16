76797052178b3dd4497830f29e75d8287739a3a0
wldntls <sddyy1004@naver.com>
2022-06-16 14:32:53 +0900

    220616_algorithm

--- algorithm/week 10/BOJ_12018.py
+++ algorithm/week 10/BOJ_12018.py
@@ -3,16 +3,16 @@
 # 가장 많은 과목수를 골라야 한다. 즉 내가 가진 포인트를 최대한 활용해서 많은 강의를 듣는것이 목적인 문제이다. 
 
 n,m=map(int,input().split()) # 과목수 n, 주어진 마일리지 m
-result=[]
+result=[] # 성준이의 마일리지를 담을 리스트
 
 for k in range(n):
-  p,l=map(int,input().split()) # 과목을 신청한 사람 p, 과목의 수강 인원이 l
+  p,l=map(int,input().split()) # 과목을 신청한 사람의 수 p, 과목의 수강 인원이 l
   mileage=list(map(int,input().split())) # 각 사람이 마일리지를 얼마나 넣었는지
   mileage.sort(reverse=True) # 내림차순으로 정렬
 
-  if p<l: # 만약 과목을 신청한 사람이 수강 인원보다 적다면 
+  if p<l: # 만약 과목을 신청한 사람이 수강 인원보다 적다면 (무조건 들을 수  있는 과목)
     result.append(1) # 마일리지를 1만 추가(예시에서는 2 4 밖에 없음)
-  else:
+  else: # 만약 그게 아니라면
     result.append(mileage[l-1]) # 수강신청 인원 마지막 사람의 마일리지를 저장
 result.sort() # 결과를 오름차순으로 정렬
 # print(result)
@@ -25,5 +25,5 @@ for i in result:
     print(cnt-1) # 과목수를 하나빼서 출력
     break
 
-if m>0: # 이건 왜하는거지..? 모든 과목수를 다 들었는대도 m이 남으면 그때는 cnt 출력!
+if m>0: # 이건 왜하는거지..? 모든 과목수를 다 들었는대도 m이 남으면 그때는 cnt 출력!, m = 0 때도 포함시켜야함
   print(cnt)
