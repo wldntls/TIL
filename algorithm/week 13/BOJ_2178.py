f813c2efd8deb21a5ce768f2a0b1186beae77e54
wldntls <sddyy1004@naver.com>
2022-06-29 11:29:03 +0900

    BOJ_2178

--- algorithm/week 13/BOJ_2178.py
+++ algorithm/week 13/BOJ_2178.py
@@ -20,7 +20,7 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            # 미로 찾기 공간을 벗어난 경우 무시
+            # 공간을 벗어난 경우 무시
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
 
